6dbb21e2a507c27293fdc1d5fa7f5cfd2e6283ef
Wilson Goode <45737196+wilsongoode@users.noreply.github.com>
2025-05-04 16:36:04 -0400

    Add documentation to PercyConfiguration

--- Sources/Percy/PercyConfiguration.swift
+++ Sources/Percy/PercyConfiguration.swift
@@ -7,9 +7,17 @@
 
 import SwiftData
 
+/// A protocol that defines your app's storage settings.
 public protocol PercyConfiguration {
+    /// The app's identifier.
     static var identifier: String { get }
+    
+    /// The app's name.
     static var name: String { get }
+    
+    /// The app's schema.
     static var schema: Schema { get }
+    
+    /// The app's schema migration plan.
     static var migrationPlan: SchemaMigrationPlan.Type { get }
 }
